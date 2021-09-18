c183846b63dc7922f0acb208d7eaf0f0c7369111
Navroop Hundal <29985828+nsh010@users.noreply.github.com>
2021-09-18 18:54:38 -0400

    updating App Structure & DB setup

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from starlette.testclient import TestClient
 
 from app.config import get_settings, Settings
-from app import main
+from app.main import create_application
 
 
 def get_settings_override():
@@ -12,7 +12,8 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
         yield test_client
 
