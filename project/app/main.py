41e1a14ebfc56692980826e3a46dddfff4921810
Navroop Hundal <29985828+nsh010@users.noreply.github.com>
2021-09-21 20:06:35 -0400

    Add Post Route

--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -10,6 +10,7 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
